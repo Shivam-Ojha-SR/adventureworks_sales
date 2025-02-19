217f2f0b2023e4d55d9cd1ef96e578a2050928ff
Shivam Ojha <sojha@spauldingridge.com>
2025-02-19 10:51:06 +0000

    Added Generic Tests for staging models. Handled '[Not Applicable]'  rows.

--- models/staging/stg_customer_data.sql
+++ models/staging/stg_customer_data.sql
@@ -2,12 +2,22 @@ with
     customer_data as (
         select
             customerkey,
-            customer_id,
-            customer,
-            city,
-            state_province,
-            country_region,
-            postal_code
+            case
+                when customer_id = '[Not Applicable]' then null else customer_id
+            end as customer_id,
+            case
+                when customer = '[Not Applicable]' then null else customer
+            end as customer,
+            case when city = '[Not Applicable]' then null else city end as city,
+            case
+                when state_province = '[Not Applicable]' then null else state_province
+            end as state_province,
+            case
+                when country_region = '[Not Applicable]' then null else country_region
+            end as country_region,
+            case
+                when postal_code = '[Not Applicable]' then null else postal_code
+            end as postal_code
         from {{ source("sales", "customer_data") }}
     )
 select *
