f56172e78a3212fd43f55ddd49e02668a0312a59
Shivam Ojha <sojha@spauldingridge.com>
2025-03-05 08:50:17 +0000

    vFinal: Checked and Verified by mentor. Moving into BI stage now! Updates: Slight update in dim files, main work done on FCT table. Also merged int_sales, daily_sales, sales_performance and rolling_7d_avg into int_sales as CTEs.

--- models/intermediate/int_sales_data.sql
+++ models/intermediate/int_sales_data.sql
@@ -1,10 +1,60 @@
-WITH sales as(
-    select * from {{ ref('stg_sales_data') }}
+WITH sales_data AS (
+    SELECT * FROM {{ ref("stg_sales_data") }}
 ),
 
-sales_data AS (
+daily_sales AS (
     SELECT 
-        *
-    FROM sales
+        orderdatekey, 
+        productkey, 
+        ROUND(SUM(salesamount), 3) AS daily_sales
+    FROM sales_data
+    GROUP BY orderdatekey, productkey
+),
+
+rolling_7_day_avg AS (
+    SELECT
+        orderdatekey,
+        productkey,
+        ROUND(
+            AVG(salesamount) OVER (
+                PARTITION BY productkey
+                ORDER BY orderdatekey
+                ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+            ), 
+            3
+        ) AS rolling_7_day_avg
+    FROM sales_data
+),
+
+product_performance AS (
+    SELECT
+        salesterritorykey,
+        productkey,
+        ROUND(SUM(salesamount), 3) AS total_sales,
+        RANK() OVER (
+            PARTITION BY salesterritorykey
+            ORDER BY SUM(salesamount) DESC
+        ) AS sales_rank
+    FROM sales_data
+    GROUP BY salesterritorykey, productkey
+),
+
+final_sales_data AS (
+    SELECT 
+        s.*,
+        d.daily_sales,
+        r.rolling_7_day_avg,
+        p.sales_rank
+    FROM sales_data s
+    LEFT JOIN daily_sales d 
+        ON s.productkey = d.productkey 
+        AND s.orderdatekey = d.orderdatekey
+    LEFT JOIN rolling_7_day_avg r 
+        ON s.productkey = r.productkey 
+        AND s.orderdatekey = r.orderdatekey
+    LEFT JOIN product_performance p 
+        ON s.productkey = p.productkey 
+        AND s.salesterritorykey = p.salesterritorykey
 )
-SELECT * FROM sales_data
+
+SELECT * FROM final_sales_data
--- models/marts/fct_sales.sql
+++ models/marts/fct_sales.sql
@@ -2,40 +2,40 @@ WITH sales_data AS (
     SELECT * FROM {{ ref('int_sales_data') }}
 ),
 
-daily_sales AS (
-    SELECT * FROM {{ ref('int_daily_sales') }}
+date AS (
+    SELECT datekey FROM {{ ref('int_date_data') }}
 ),
 
-weekly_sales AS (
-    SELECT * FROM {{ ref('int_weekly_sales') }}
+customer AS (
+    SELECT customerkey FROM {{ ref('int_customer_data') }}
 ),
 
-monthly_sales AS (
-    SELECT * FROM {{ ref('int_monthly_sales') }}
-), 
-
-quarterly_sales AS (
-    SELECT * FROM {{ ref('int_quarterly_sales') }}
+product AS (
+    SELECT productkey FROM {{ ref('int_product_data') }}
 ),
 
-rolling_7_day_avg AS (
-    SELECT * FROM {{ ref('int_rolling_7d_avg') }}
+reseller AS (
+    SELECT resellerkey FROM {{ ref('int_reseller_data') }}
 ),
 
-product_performance AS (
-    SELECT * FROM {{ ref('int_product_performance') }}
+sales_territory AS (
+    SELECT salesterritorykey FROM {{ ref('int_sales_territory_data') }}
 ),
 
 fct_sales AS (
     SELECT
         sales_data.salesorderlinekey,
-        sales_data.resellerkey,
-        sales_data.customerkey,
-        sales_data.productkey,
+        
+        -- FK for dims
         sales_data.orderdatekey,
         sales_data.duedatekey,
         sales_data.shipdatekey,
+        sales_data.customerkey,
+        sales_data.productkey,
+        sales_data.resellerkey,
         sales_data.salesterritorykey,
+
+        -- quantitative stuff
         sales_data.orderquantity,
         sales_data.unitprice,
         sales_data.extendedamount,
@@ -43,31 +43,16 @@ fct_sales AS (
         sales_data.productstandardcost,
         sales_data.totalproductcost,
         sales_data.salesamount,
-        daily_sales.daily_sales,
-        weekly_sales.weekly_sales,
-        monthly_sales.monthly_sales,
-        quarterly_sales.quarterly_sales,
-        rolling_7_day_avg.rolling_7_day_avg,
-        product_performance.sales_rank
+        sales_data.daily_sales,
+        sales_data.rolling_7_day_avg,
+        sales_data.sales_rank
     FROM sales_data
-    LEFT JOIN daily_sales 
-        ON sales_data.productkey = daily_sales.productkey 
-        AND sales_data.orderdatekey = daily_sales.orderdatekey
-    LEFT JOIN weekly_sales 
-        ON sales_data.productkey = weekly_sales.productkey 
-        AND sales_data.orderdatekey = weekly_sales.orderdatekey
-    LEFT JOIN monthly_sales 
-        ON sales_data.productkey = monthly_sales.productkey 
-        AND sales_data.orderdatekey = monthly_sales.orderdatekey
-    LEFT JOIN quarterly_sales 
-        ON sales_data.productkey = quarterly_sales.productkey 
-        AND sales_data.orderdatekey = quarterly_sales.orderdatekey
-    LEFT JOIN rolling_7_day_avg 
-        ON sales_data.productkey = rolling_7_day_avg.productkey 
-        AND sales_data.orderdatekey = rolling_7_day_avg.orderdatekey
-    LEFT JOIN product_performance 
-        ON sales_data.productkey = product_performance.productkey 
-        AND sales_data.salesterritorykey = product_performance.salesterritorykey
+
+    LEFT JOIN date ON sales_data.orderdatekey = date.datekey
+    LEFT JOIN customer ON sales_data.customerkey = customer.customerkey
+    LEFT JOIN product ON sales_data.productkey = product.productkey
+    LEFT JOIN reseller ON sales_data.resellerkey = reseller.resellerkey
+    LEFT JOIN sales_territory ON sales_data.salesterritorykey = sales_territory.salesterritorykey
 )
 
 SELECT * FROM fct_sales
